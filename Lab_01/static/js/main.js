599d9ba649b34652f8a153a21052dcc79ecc5140
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-03-31 01:13:05 -0300

    Basic lab created

---
+++ Lab_01/static/js/main.js
@@ -0,0 +1,15 @@
+function closePanel() {
+    panel = document.getElementById('controlPanel-bg');
+    ticket = document.getElementById('lateral-CP');
+    
+    panel.style.display = 'none';
+    ticket.style.display = 'block';
+}
+
+function closeTicket() {
+    panel = document.getElementById('controlPanel-bg');
+    ticket = document.getElementById('lateral-CP');
+    
+    panel.style.display = 'block';
+    ticket.style.display = 'none';
+}
